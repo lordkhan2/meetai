281f943437a537992a04eb84ec6b4ca459715849
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2025-07-16 11:39:48 +1000

    Meeting Page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -9,6 +9,21 @@ import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
+    remove: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        const [removedMeeting] = await db
+            .delete(meetings)
+            .where(and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)))
+            .returning();
+
+        if (!removedMeeting) {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Meeting not found",
+            });
+        }
+
+        return removedMeeting;
+    }),
     update: protectedProcedure.input(meetingsUpdateSchema).mutation(async ({ ctx, input }) => {
         const [updatedMeetings] = await db
             .update(meetings)
@@ -35,8 +50,9 @@ export const meetingsRouter = createTRPCRouter({
     }),
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
         const [existingMeeting] = await db
-            .select({ ...getTableColumns(meetings) })
+            .select({ ...getTableColumns(meetings), agent: agents, duration: sql<number>`EXTRACT(EPOC FROM (ended_at - started_at))`.as("duration") })
             .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
             .where(and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)));
 
         if (!existingMeeting) {
